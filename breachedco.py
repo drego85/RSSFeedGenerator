54086d980fa2428ab5b77debd9c157cea5147764
Andrea Draghetti <drego85@draghetti.it>
2022-04-01 12:35:08 +0200

    Introduced Breached.co

---
+++ breachedco.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# This file is part of RSS Generator Feed.
+#
+# RSS Feed Generator was made with ♥ by Andrea Draghetti
+#
+# This file may be licensed under the terms of of the
+# GNU General Public License Version 3 (the ``GPL'').
+#
+import os
+import Config
+import requests
+from lxml import etree as ET
+from bs4 import BeautifulSoup
+from readability import Document
+from time import gmtime, strftime
+
+# User Agent MSIE 11.0 (Win 10)
+header_desktop = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; MATBJS; rv:11.0) like Gecko",
+                  "Accept-Language": "it"}
+
+timeoutconnection = 120
+rssfile = Config.outputpath + "breachedco.xml"
+list_of_articles = []
+
+list_of_excluede_articles = ["https://breached.co/archive/index.php/thread-847.html",
+                             "https://breached.co/archive/index.php/thread-262.html"]
+
+
+def make_feed():
+    root = ET.Element("rss")
+    root.set("version", "2.0")
+
+    channel = ET.SubElement(root, "channel")
+
+    title = ET.SubElement(channel, "title")
+    title.text = "Breached.co"
+
+    link = ET.SubElement(channel, "link")
+    link.text = "http://rss.draghetti.it"
+
+    description = ET.SubElement(channel, "description")
+    description.text = "RSS feed dei Data Leak pubblicati su Breached.co"
+
+    language = ET.SubElement(channel, "language")
+    language.text = "it-IT"
+
+    generator = ET.SubElement(channel, "generator")
+    generator.text = "Breached.co by Andrea Draghetti"
+
+    tree = ET.ElementTree(root)
+    tree.write(rssfile, pretty_print=True, xml_declaration=True, encoding="UTF-8")
+
+
+def add_feed(titlefeed, descriptionfeed, linkfeed):
+    parser = ET.XMLParser(remove_blank_text=True)
+    tree = ET.parse(rssfile, parser)
+    channel = tree.getroot()
+
+    # Escludo eventuali duplicati in base al link
+    for i in channel.findall(".//link"):
+        if (i.text == linkfeed):
+            return
+
+    item = ET.SubElement(channel, "item")
+
+    title = ET.SubElement(item, "title")
+    title.text = titlefeed
+
+    link = ET.SubElement(item, "link")
+    link.text = linkfeed
+
+    description = ET.SubElement(item, "description")
+    description.text = descriptionfeed
+
+    pubDate = ET.SubElement(item, "pubDate")
+    pubDate.text = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+
+    channel.find(".//generator").addnext(item)
+
+    tree = ET.ElementTree(channel)
+    tree.write(rssfile, pretty_print=True, xml_declaration=True, encoding="UTF-8")
+
+
+def scrap_breachedco(url):
+    pagedesktop = requests.get(url, headers=header_desktop, timeout=timeoutconnection)
+    soupdesktop = BeautifulSoup(pagedesktop.text, "html.parser")
+
+    try:
+        for div in soupdesktop.find_all("div", attrs={"class": "threadlist"}):
+            for ol in div.find("ol").find_all("li"):
+                for link in ol.find_all("a", href=True):
+                    article_link = link["href"]
+                    if article_link not in list_of_excluede_articles:
+                        list_of_articles.append(article_link)
+    except:
+        pass
+
+
+def main():
+    list_of_urls = ["https://breached.co/archive/index.php/forum-7.html",
+                    "https://breached.co/archive/index.php/forum-24.html"]
+
+    for url in list_of_urls:
+        scrap_breachedco(url)
+
+    # Se non esiste localmente un file XML procedo a crearlo.
+    if os.path.exists(rssfile) is not True:
+        make_feed()
+
+    # Analizzo ogni singolo articolo rilevato
+    for urlarticolo in list_of_articles:
+        response = requests.get(urlarticolo, headers=header_desktop, timeout=timeoutconnection)
+
+        description = Document(response.text).summary()
+        title = Document(response.text).short_title()
+
+        add_feed(title, description, urlarticolo)
+
+
+if __name__ == "__main__":
+    main()
